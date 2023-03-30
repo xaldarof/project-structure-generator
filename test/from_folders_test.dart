6aa645cfcb76fb1474bdc5488917229b45eb5124
Temur <t.kholdarov@baik.uz>
2023-03-30 11:28:30 +0500

    fix

--- test/from_folders_test.dart
+++ test/from_folders_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:test/test.dart';
 
 
---
+++ test/to_folders_test.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+
+void main() {
+  final file = File("${Directory.current.path}/test/fake_json/to_folders.json");
+
+  test('description', () {
+    //
+  });
+}
